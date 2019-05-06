c38ef6edc9c284c39f83aafc454cb10f88f4c756
Lucia Pereira <luhelenapereira@gmail.com>
2019-05-05 23:31:13 -0300

    alteração classe disciplina; interface grafica na activity nova disciplina; implementacao classe novoplanejamentodisciplina.

--- app/src/main/java/dcc196/ufjf/br/trabalho1/Disciplina.java
+++ app/src/main/java/dcc196/ufjf/br/trabalho1/Disciplina.java
@@ -1,29 +1,83 @@
 package dcc196.ufjf.br.trabalho1;
 
+import java.util.ArrayList;
+
 public class Disciplina {
 
-    private final String titulo;
-    private final int horas;
-    private  final String area;
+    private String titulo;
+    private String horas;
+    private  String area;
+    private ArrayList<Disciplina> disc = new ArrayList<>();
 
 
-    public Disciplina(int horas, String area, String titulo) {
+    public Disciplina(String horas, String area, String titulo) {
         this.horas = horas;
         this.area = area;
         this.titulo =  titulo;
+    }
+
+    public Disciplina() {
+        if (disc.isEmpty()) {
+            criarDisciplina();
+        }
+
     }
 
     public String getTitulo() {
         return titulo;
     }
 
-    public int getHoras() {
+    public String getHoras() {
         return horas;
     }
 
     public String getArea() {
         return area;
     }
+
+    @Override
+    public String toString() {
+        return titulo;
+    }
+
+
+    public ArrayList<Disciplina> getDisc(){
+        return disc;
+    }
+
+    private void  criarDisciplina()
+    {
+        disc.add(new Disciplina("10", "exatas", "matematica"));
+        disc.add(new Disciplina("11","humanas","portugues"));
+
+    }
+
+    public void adicionarDisciplinas(Disciplina d)
+    {
+        disc.add(d);
+    }
+
+    public ArrayList<Disciplina> getDisciplina() {
+
+        return disc;
+    }
+
+    public void addDisciplina(Disciplina d) {
+
+        disc.add(d);
+
+    }
+    public Disciplina getParticipantes(String titulo) {
+        for (Disciplina d : this.getDisciplina()) {
+            if (d.getTitulo().equals(titulo)) {
+                return d;
+            }
+        }
+        return null;
+    }
+
+
+
 
 
 }
